7ce646f7480dd9e12518b6c706be70fb610589dc
Monica <monicasucianto123@gmail.com>
2022-12-23 03:42:30 +0800

    Submission 2

--- app/src/main/java/com/example/randomnews/ui/home/HomeActivity.kt
+++ app/src/main/java/com/example/randomnews/ui/home/HomeActivity.kt
@@ -6,7 +6,6 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
